a5cc3e981a2c8dd51f1befbe895d686338feab61
palianjali <pali60658@gmail.com>
2025-04-23 17:39:26 +0530

    1

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -9,9 +9,9 @@ const About = () => {
         <Title text1={'ABOUT'} text2={'US'} />
       </div>
 
-      <div className='my-10 flex flex-col md:flex-row gap-16'>
+      <div className='my-10 flex flex-col md:flex-row gap-16 shadow-lg'>
         <img 
-        className='w-full md:max-w-[450px]'
+        className='w-full md:max-w-[450px] '
         src={assets.about_us_img} alt="" />
 
         <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
@@ -21,11 +21,11 @@ const About = () => {
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere id aut veritatis soluta fugiat optio, odit eius pariatur animi odio obcaecati, reprehenderit nesciunt? Magnam, deserunt incidunt vel aspernatur distinctio totam.</p>
         </div>
       </div>
-      <div className='text-xl py-4'>
+      <div className='text-xl py-4 '>
         <Title text1={'WHY'} text2={'CHOOSE US'}/>
       </div>
 
-      <div className='text-gray-600 flex flex-col md:flex-row text-sm mb-20'> 
+      <div className='text-gray-600 flex flex-col md:flex-row text-sm mb-20 hover:shadow-lg'> 
         <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
           <b>Quality Assurance:</b>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati eius at vitae blanditiis odio, sequi temporibus animi quidem necessitatibus amet repellendus culpa voluptatibus aut nisi, quaerat esse laudantium, recusandae nobis?</p>
